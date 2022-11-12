c3a698cb7beef464d09ab313304b93d87d170d9f
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2022-11-12 11:27:13 +0100

    Better comment

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ async function init(token) {
             description: 'I hate discord so much you cannot believe it',
             type: 1,
         }])
+        //Send the url (Beceause badge for noob)
         console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
     })
     // Stupid ass interaction creation event
     stupidAssBot.on('interactionCreate', async (interaction) => {
+        //Check if the interaction data is lol command
         if (interaction?.data?.name === 'lol') {
             await interaction.createMessage({
+                //Send the ass command
                 content: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don\'t care what humans think is impossible.'
             })
             console.log('Self destructing...')
